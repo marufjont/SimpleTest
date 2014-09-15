d21258c3f995fd609b9655fb3ec90c1fe2d96418
marufjont <marufjont@gmail.com>
2014-09-15 15:44:00 -0400

    Initial commit

---
+++ src/test/java/SimpleTestRun.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
+
+/**
+ * Created by Maruf on 9/15/2014.
+ */
+public class SimpleTestRun {
+    @Test
+    public void testRun() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("seach");
+        driver.findElement(By.name("q")).submit();
+        assertTrue(true);
+    }
+}
